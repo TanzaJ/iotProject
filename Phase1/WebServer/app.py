032f0dd9611b66c98a757746f5f54600300d2794
JamesKawashima <jameskawashima289@gmail.com>
2023-09-29 21:31:41 -0400

    Added phase1

---
+++ Phase1/WebServer/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+led = 17
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT, initial=GPIO.LOW)
+
+@app.route('/on')
+def on():
+    GPIO.output(led, GPIO.HIGH)
+    return render_template('on.html')
+
+@app.route('/off')
+def off():
+    GPIO.output(led, GPIO.LOW)
+    return render_template('off.html')
