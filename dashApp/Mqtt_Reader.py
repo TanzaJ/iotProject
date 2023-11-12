c8e530950766915eaee69caa155aca87915f3300
JamesKawashima <jameskawashima289@gmail.com>
2023-11-12 13:05:37 -0500

    mqtt fix attempt 10

--- dashApp/Mqtt_Reader.py
+++ dashApp/Mqtt_Reader.py
@@ -4,44 +4,40 @@ Thomas Varnish (https://github.com/tvarnish), (https://www.instructables.com/mem
 Written for my Instructable - "How to use MQTT with the Raspberry Pi and ESP8266"
 """
 import paho.mqtt.client as mqtt
+import time
 
+def on_connect(client, usedata, flags, rc):
+    if rc == 0:
+        print("client is connected")
+        global connected
+        connected = True
+    else:
+        print("connection failded")
+        
+def on_message(client, userdata, message):
+    print(str(message.topic) + ": " + str(message.payload.decode("utf-8")))
+
+connected = False
+MessageReceived = False
 photoData = "0"
+port = 1883
 mqtt_topic = "Light_Sensor"
 mqtt_broker_ip = "192.168.0.116"
 
 def getValue():
     return photoData
 
-while True:
-    # Don't forget to change the variables for the MQTT broker!
+client = mqtt.Client("Light REader")
 
-    # These functions handle what happens when the MQTT client connects
-    # to the broker, and what happens then the topic receives a message
-    def on_connect(userdata, flags, rc):
-        # rc is the error code returned when connecting to the broker
-        print ("Connected!", str(rc))
-        # Once the client has connected to the broker, subscribe to the topic
-        self.client.subscribe(mqtt_topic)
-        
-    def on_message(userdata, msg):
-        # This function is called everytime the topic is published to.
-        # If you want to check each message, and do something depending on
-        # the content, the code to do this should be run in this function
-        photoData = "" + str(msg.payload)
-        print ("Topic: ", msg.topic + "\nMessage: " + str(msg.payload))
-        
-        # The message itself is stored in the msg variable
-        # and details about who sent it are stored in userdata
-    client = mqtt.Client()
-    client.connect(mqtt_broker_ip, 1883)
-    # Here, we are telling the client which functions are to be run
-    # on connecting, and on receiving a message
-    client.on_connect = on_connect
-    client.on_message = on_message
+client.on_connect = on_connect
+client.connect(mqtt_broker_ip, port=port)
+client.loop_start()
+client.subscribe(mqtt_topic)
+while connected != True:
+    time.sleep(0.2)
 
-    # Once everything has been set up, we can (finally) connect to the broker
-    # 1883 is the listener port that the MQTT broker is using
-
-
-    # Once we have told the client to connect, let the client object run itself
+while MessageReceived != True:
+    time.sleep(0.2)
+client.on_message = on_message
+client.loop_stop()
 
